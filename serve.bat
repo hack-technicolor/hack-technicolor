5459a07ec179f61afa23b4d025a3bf50108be612
flywire <flywire0@gmail.com>
2019-09-18 00:42:07 +1000

    Windows library path for 7fc0a9f

--- serve.bat
+++ serve.bat
@@ -1,4 +1,5 @@
 @echo off
+set path=%path%;C:\Program Files\GTK3-Runtime Win64\bin
 rem Default Install ie 'Just Me', default folder, Don't add to path or register as system python
 set AnacondaPath=%USERPROFILE%\Anaconda3
 call %AnacondaPath%\Scripts\activate.bat %AnacondaPath%
